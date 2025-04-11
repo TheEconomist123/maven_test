c71c0a117dbf9722256f6fc874707092ca0053ff
zhouzhengzhi <2248869976@qq.com>
2025-04-11 13:56:33 +0800

    maven 依赖问题

---
+++ maven_A/src/test/java/com/zhouzz/maven/test/DruidTest.java
@@ -0,0 +1,21 @@
+package com.zhouzz.maven.test;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author zhouzz
+ * @version 1.0
+ * @date 2025/4/11 13:47
+ */
+public class DruidTest {
+
+
+    @Test
+    public void testDruid() {
+
+        //这是maven_B 里面引入的依赖
+        DruidDataSource ds = new DruidDataSource();
+
+    }
+}
